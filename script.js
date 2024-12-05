9b4822ea54588891d6fec152868c51fb253d5526
Suvo Ghosh <suvosh663@gmail.com>
2024-12-05 22:55:03 +0530

    delete some comments

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const container = document.querySelector('.container')
 const paraContainer = document.querySelector('.para-container')
 
 showNotes()
-document.querySelector('button').addEventListener('click' , ()=>{
+document.querySelector('button').addEventListener('click', () => {
     let notesPara = document.createElement('p')
     notesPara.className = 'note-para'
-    notesPara.setAttribute('contentEditable','true')  
+    notesPara.setAttribute('contentEditable', 'true')
     paraContainer.appendChild(notesPara)
 
     let deleteImg = document.createElement('img')
@@ -13,31 +13,31 @@ document.querySelector('button').addEventListener('click' , ()=>{
     notesPara.appendChild(deleteImg)
 })
 
-paraContainer.addEventListener('click' , (e)=>{
-    if(e.target.tagName === 'IMG'){
+paraContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
         e.target.parentElement.remove()
         updateStorage()
-    }else if(e.target.tagName === 'P'){ 
+    } else if (e.target.tagName === 'P') {
         notes = document.querySelectorAll('.note-para')
         notes.forEach(ele => {
-            ele.onkeyup = function(){
-                updateStorage()   
+            ele.onkeyup = function () {
+                updateStorage()
             }
         });
     }
 })
 
-document.addEventListener('keydown' , (e)=>{
-    if(e.key === 'Enter'){
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
         document.execCommand('insertLineBreak')
         e.preventDefault()
     }
 })
 
-function updateStorage(){
-    localStorage.setItem('notesContainer',paraContainer.innerHTML)
+function updateStorage() {
+    localStorage.setItem('notesContainer', paraContainer.innerHTML)
 }
 
-function showNotes(){
+function showNotes() {
     paraContainer.innerHTML = localStorage.getItem('notesContainer')
 }
